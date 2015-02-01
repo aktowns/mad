889f68e0fcde857d3123389529a15b4b6bc2d523
Ashley Towns <ashleyis@me.com>
2015-02-01 20:17:30 +1100

    move theme out to a hash

--- lib/mad/theme.rb
+++ lib/mad/theme.rb
@@ -12,37 +12,39 @@ module Mad
     YELLOW  = Termbox::TB_YELLOW
     CYAN    = Termbox::TB_CYAN
 
+    DEFAULT_THEME = {
+      keyword:                  BLUE,
+      name:                     CYAN,
+      name_builtin:             YELLOW,
+      name_constant:            RED,
+      name_function:            YELLOW,
+      name_variable_instance:   BLACK,
+      name_variable_global:     BLACK,
+      name_class:               GREEN,
+      operator:                 CYAN,
+      punctuation:              CYAN,
+      name_namespace:           CYAN,
+      keyword_pseudo:           CYAN,
+
+      literal_string_backtick:  RED,
+      literal_string_symbol:    RED,
+      literal_string_double:    RED,
+      literal_string_single:    RED,
+      literal_number_hex:       RED,
+      literal_number_integer:   RED,
+      literal_string_interpol:  RED,
+      literal_string_regex:     RED,
+
+      comment_single:           RED,
+      error:                    CYAN,
+      text:                     CYAN
+    }
+
     def self.get_colour(kind)
-      case kind
-      when :keyword                 then BLUE         # ie class
-      when :name                    then CYAN
-      when :name_builtin            then YELLOW       # ie require
-      when :name_constant           then RED          # ie class blah < *<NAME>*
-      when :name_function           then YELLOW       # ie def *<NAME>*
-      when :name_variable_instance  then BLACK        # ie @blah
-      when :name_variable_global    then BLACK
-      when :name_class              then GREEN        # ie class *<NAME>*
-      when :operator                then CYAN
-      when :punctuation             then CYAN
-      when :name_namespace          then CYAN
-      when :keyword_pseudo          then CYAN
+      colour = DEFAULT_THEME[kind]
+      fail(ThemeKeywordUnknown, kind) if colour.nil? && !kind.nil?
 
-      when :literal_string_backtick then RED
-      when :literal_string_symbol   then RED
-      when :literal_string_double   then RED
-      when :literal_string_single   then RED
-      when :literal_number_hex      then RED
-      when :literal_number_integer  then RED
-      when :literal_string_interpol then RED
-      when :literal_string_regex    then RED
-
-      when :comment_single          then RED
-      when :error                   then CYAN
-      when :text                    then CYAN
-      else
-        fail(ThemeKeywordUnknown, kind) unless kind.nil?
-        Termbox::TB_CYAN
-      end
+      colour || CYAN
     end
   end
 end
