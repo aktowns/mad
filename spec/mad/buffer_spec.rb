a3f3675b5a2dd929f9501e8b3724ab7d980cbcf8
Ashley Towns <ashleyis@me.com>
2015-02-01 18:51:12 +1100

    initial specs

---
+++ spec/mad/buffer_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Mad::Buffer do
+  it 'can read from a file'
+  it 'returns the correct line length'
+  it 'returns the correct character at a position'
+  it 'can highlight source code'
+  it 'deletes a line at a given index'
+  it 'inserts a line at a given index'
+end
