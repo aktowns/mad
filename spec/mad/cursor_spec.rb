004e111508362d1eb09dc19235cb7dd2e42c30d4
Ashley Towns <ashleyis@me.com>
2015-02-01 20:00:45 +1100

    added tests for cursor

---
+++ spec/mad/cursor_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+module Mad
+  describe Cursor do
+    before(:each) do
+      @cursor = Cursor.new
+    end
+
+    it 'starts at 0,0' do
+      expect(@cursor.pos.x).to eq(0)
+      expect(@cursor.pos.y).to eq(0)
+    end
+
+    it 'can have a set position' do
+      @cursor.set(10, 20)
+
+      expect(@cursor.pos.x).to eq(10)
+      expect(@cursor.pos.y).to eq(20)
+    end
+
+    it 'moves up' do
+      @cursor.pos.y = 10
+      @cursor.up
+
+      expect(@cursor.pos.y).to eq(9)
+    end
+
+    it 'moves down' do
+      @cursor.down
+
+      expect(@cursor.pos.y).to eq(1)
+    end
+
+    it 'moves left' do
+      @cursor.pos.x = 10
+      @cursor.left
+
+      expect(@cursor.pos.x).to eq(9)
+    end
+
+    it 'moves right' do
+      @cursor.right
+
+      expect(@cursor.pos.x).to eq(1)
+    end
+
+    it 'wont move into negative y axis' do
+      @cursor.up
+
+      expect(@cursor.pos.x).to eq(0)
+    end
+
+    it 'wont move into negative x axis' do
+      @cursor.left
+
+      expect(@cursor.pos.x).to eq(0)
+    end
+  end
+end
